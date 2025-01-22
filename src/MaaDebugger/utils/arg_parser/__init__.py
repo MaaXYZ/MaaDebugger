2d405bd79fca4522a65a4af45df3ab81f22d0584
不留 <weinibuliu@outlook.com>
2025-01-22 22:11:17 +0800

    feat: 添加更多命令行参数

--- src/MaaDebugger/utils/arg_parser/__init__.py
+++ src/MaaDebugger/utils/arg_parser/__init__.py
@@ -6,7 +6,9 @@ from ..port_checker import PortChecker
 
 class ArgParser:
     def __init__(self) -> None:
-        self.parser = argparse.ArgumentParser(description="A debugger specifically for MaaFramework.")
+        self.parser = argparse.ArgumentParser(
+            description="A debugger specifically for MaaFramework."
+        )
         self._add_argument()
         self.args = self.parser.parse_args()
 
@@ -14,9 +16,32 @@ class ArgParser:
         """
         Add command line arguments to the parser.
         """
-        self.parser.add_argument("--port", type=int, help="run port")
+        self.parser.add_argument(
+            "--port",
+            type=int,
+            help="Run on which port",
+            default=None,
+        )
+        self.parser.add_argument(
+            "--host",
+            type=str,
+            help="When the value is 'localhost', only the local machine can access it. If you want to change this,you can set value as '0.0.0.0'. (Default: localhost)",
+            default="localhost",
+        )
+        self.parser.add_argument(
+            "--hide",
+            type=str,
+            help="Whether NOT automatically open the UI in a browser tab. (Default: False)",
+            default=False,
+        )
 
-    
+        self.parser.add_argument(
+            "--dark",
+            type=str,
+            help="Whether enabel dark mode.None means auto. (Default: None)",
+            default=None,
+        )
+
     def get_port(self) -> int:
         """
         Determine the port to use based on the provided arguments.
@@ -32,3 +57,35 @@ class ArgParser:
         else:
             port = PortChecker.find_available_port(8011)
         return port
+
+    def get_host(self) -> str:
+        """
+        When the value is 'localhost' (or '127.0.0.1'), only the local machine can access it.
+        If you want to change this,you can set value as '0.0.0.0'
+        """
+
+        host = self.args.host
+
+        if host in ["localhost", "127.0.0.1"]:
+            print("NOTICE: Only the local machine can access MaaDebugger.")
+        else:
+            print("WARNING: All devices in the LAN can access MaaDebugger.")
+        return host
+
+    def get_hide(self) -> bool:
+        hide = self.args.hide
+
+        if hide in ["True", "true"]:
+            return True
+        else:
+            return False
+
+    def get_dark(self) -> Optional[bool]:
+        dark = self.args.dark
+
+        if dark in ["True", "true"]:
+            return True
+        elif dark in ["False", "false"]:
+            return False
+        else:
+            return None
