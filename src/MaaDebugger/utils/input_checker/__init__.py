92ea58d20d95dae4fa620f8672fdf46d4335e2e7
不留 <weinibuliu@outlook.com>
2025-02-10 20:17:44 +0800

    refactor: 调整部分样式与布局；对 json 字符串进行校验 (#32)
    感觉还可以加些 tooltip ，但是俺没有头绪（

---
+++ src/MaaDebugger/utils/input_checker/__init__.py
@@ -0,0 +1,55 @@
+import json
+from pathlib import Path
+from typing import Optional
+
+
+def hwnd(data: str) -> Optional[str]:
+    if not data:
+        return
+
+    try:
+        int(data, 16)
+    except:
+        return "Please input hexadecimal numbers."
+
+
+def json_style_str(data: str) -> Optional[str]:
+    if not data:
+        return
+
+    try:
+        json.loads(data)
+    except json.decoder.JSONDecodeError as e:
+        return f"JSONDecodeError: {e}"
+    except Exception as e:
+        return str(e)
+
+
+def paths_exist(data: str) -> Optional[str]:
+    if not data:
+        return
+
+    not_exist_paths = []
+    not_pipe_paths = []
+    msg = ""
+
+    paths = [Path(p) for p in data.split("\n") if p]
+    for p in paths:
+        if p.exists():
+            pipe_dir = Path(p, "pipeline")
+            if not pipe_dir.is_dir():
+                not_pipe_paths.append(str(p))
+        else:
+            not_exist_paths.append(str(p))
+
+    if not_exist_paths:
+        if len(not_pipe_paths) == 1:
+            msg += f"Path not exist: {not_exist_paths}"
+        else:
+            msg += f"Paths not exist: {not_exist_paths}"
+
+    if not_pipe_paths:
+        msg += f" Pipeline dir not in: {not_pipe_paths}"
+
+    if msg:
+        return msg
