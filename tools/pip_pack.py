3c4be5a673baf188618afafdf6213c3e0982a273
不留 <weinibuliu@outlook.com>
2025-03-19 17:50:12 +0800

    feat: 加入 `__version__.py` 标注版本信息

--- tools/pip_pack.py
+++ tools/pip_pack.py
@@ -1,4 +1,5 @@
 import sys
+
 import tomlkit
 
 
@@ -17,8 +18,27 @@ def set_toml_ver():
         tomlkit.dump(py_project, f)
 
 
+def set_python_ver():
+    VER_FILE = "src/MaaDebugger/__version__.py"
+
+    tag_name: str = sys.argv[1]
+    ver = (
+        tag_name.lstrip("v")
+        .replace("-", "")
+        .replace("alpha.", "a")
+        .replace("beta.", "b")
+    )
+
+    with open(VER_FILE, "w", encoding="utf-8") as f:
+        f.write(f'version = "{ver}"\n')
+        f.write(f'tag_name = "{tag_name}"\n')
+
+    print(f"version = {ver} | tag_name = {tag_name}")
+
+
 def main():
     set_toml_ver()
+    set_python_ver()
 
 
 if __name__ == "__main__":
