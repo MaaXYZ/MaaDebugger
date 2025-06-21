d4a9d40497201fb37b15ff3b47a8f464e94f124b
不留 <weinibuliu@outlook.com>
2025-06-22 00:23:08 +0800

    feat: 重构 `runtime_control.py` 以实现 item 分页; 优化 item 相关体验 (#108)
    本 PR 主要实现了 item 分页 #94
    
    >[!NOTE]
    item 分页功能默认是禁用的。在 #62 完成前，需要开发者手动修改
    `./src/MaaDebugger/webpage/index_page/runtime_control.py` 文件的
    `PER_PAGE_ITEM_NUM` 常量启用。
    
    同时优化了一些内容：
    - item 现在将在完全生成后才可见
    - 当 reco_id == 0 时，不执行回调（直接 return ）
    - 允许用户自行选择节点输出顺序 #98 #99
    
    需要进行以下测试：
    - [x] 直接启动后，修改前后的行为上是否大体上一致？
    - [x] 修改 `./src/MaaDebugger/webpage/index_page/runtime_control.py` 定义的
    `PER_PAGE_ITEM_NUM` 常量，观察其行为是否正常。
    
    还需要完成的内容：
    - [x] 完善代码注释
    - 完善文档 (新建 PR)
    - 完成 #62 (新建 PR)

--- src/MaaDebugger/webpage/index_page/runtime_control.py
+++ src/MaaDebugger/webpage/index_page/runtime_control.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
+from dataclasses import dataclass
 from typing import Callable, Optional
 import asyncio
 
-from nicegui import ui
+from nicegui import app, ui
 from nicegui.binding import bindable_dataclass
 from maa.notification_handler import NotificationHandler, NotificationType
 
@@ -11,53 +12,90 @@ from ...webpage.components.status_indicator import Status, StatusIndicator
 from ...webpage.reco_page import RecoData
 from .global_status import GlobalStatus
 
+PAGINATION_DOCS_URL = "https://github.com"  # wait for complete
+
+STORAGE = app.storage.general
+
+# Set None to disable pagination or warning
+PER_PAGE_ITEM_NUM: Optional[int] = None
+# When the pagination is disabled and the item number reaches this value, a warning will be displayed.
+ITEM_NUMBER_WARING: Optional[int] = 400
+
+
+@bindable_dataclass
+class ItemData:
+    col: int
+    row: int
+    name: str
+    reco_id: int = 0
+    status: Status = Status.PENDING
+
+
+@dataclass
+class ListData:
+    row_len: int
+    current: str
+    list_to_reco: list[str]
+
 
 def main():
-    Controls.recognition_row.register()
+    reco_data = RecognitionRow()
+    reco_data.init_elements()
+
+
+class MyNotificationHandler(NotificationHandler):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.on_next_list_starting: Optional[Callable] = None
+        self.on_recognized: Optional[Callable] = None
+
+    def on_node_next_list(
+        self,
+        noti_type: NotificationType,
+        detail: NotificationHandler.NodeNextListDetail,
+    ):
+        if noti_type != NotificationType.Starting:
+            return
+
+        if self.on_next_list_starting is not None:
+            self.on_next_list_starting(detail.name, detail.next_list)
+
+    def on_node_recognition(
+        self,
+        noti_type: NotificationType,
+        detail: NotificationHandler.NodeRecognitionDetail,
+    ):
+        if (
+            noti_type != NotificationType.Succeeded
+            and noti_type != NotificationType.Failed
+        ):
+            return
+
+        if self.on_recognized is not None:
+            self.on_recognized(
+                detail.reco_id, detail.name, noti_type == NotificationType.Succeeded
+            )
 
 
 class RecognitionRow:
-
-    class MyNotificationHandler(NotificationHandler):
-
-        def __init__(self) -> None:
-            super().__init__()
-
-            self.on_next_list_starting: Optional[Callable] = None
-            self.on_recognized: Optional[Callable] = None
-
-        def on_node_next_list(
-            self,
-            noti_type: NotificationType,
-            detail: NotificationHandler.NodeNextListDetail,
-        ):
-            if noti_type != NotificationType.Starting:
-                return
-
-            if self.on_next_list_starting is not None:
-                self.on_next_list_starting(detail.name, detail.next_list)
-
-        def on_node_recognition(
-            self,
-            noti_type: NotificationType,
-            detail: NotificationHandler.NodeRecognitionDetail,
-        ):
-            if (
-                noti_type != NotificationType.Succeeded
-                and noti_type != NotificationType.Failed
-            ):
-                return
-
-            if self.on_recognized is not None:
-                self.on_recognized(
-                    detail.reco_id, detail.name, noti_type == NotificationType.Succeeded
-                )
-
     def __init__(self) -> None:
         self.row_len = 0
         self.data = defaultdict(dict)
+        self.list_data_map: dict[int, ListData] = {}
 
-    def register(self):
+        self.register_notification_handler()
+
+    def register_notification_handler(self):
+        """Register the custom notification handler to maafw."""
+        self.notification_handler = MyNotificationHandler()
+        self.notification_handler.on_next_list_starting = self.on_next_list_starting
+        self.notification_handler.on_recognized = self.on_recognized
+
+        maafw.notification_handler = self.notification_handler
+
+    def init_elements(self):
+        """Initialize the UI elements."""
         with ui.row():
             ui.button("Clear Items", icon="remove", on_click=self.clear_items).props(
                 "no-caps"
@@ -65,60 +103,181 @@ class RecognitionRow:
             ui.button(
                 "Clear Items and Cache", icon="delete_forever", on_click=self.clear
             ).props("no-caps").bind_enabled_from(
-                GlobalStatus,
-                "task_running",
-                lambda x: x == Status.FAILED or x == Status.SUCCEEDED,
+                GlobalStatus, "task_running", lambda x: x != Status.RUNNING
+            )
+            self.reverse_switch = (
+                ui.switch(
+                    "Reverse",
+                    value=STORAGE.get("items-reverse", True),
+                    on_change=lambda x: self.on_reverse_switch_change(x.value),
+                )
+                .tooltip("Switch this will clear all items and cache.")
+                .bind_enabled_from(
+                    GlobalStatus, "task_running", lambda x: x != Status.RUNNING
+                )
             )
 
-        self.row = ui.row(align_items="start")
+        self.pagination = ui.pagination(1, 1)
 
-        self.notification_handler = self.MyNotificationHandler()
-        self.notification_handler.on_next_list_starting = self.on_next_list_starting
-        self.notification_handler.on_recognized = self.on_recognized
+        self.homepage_row = ui.row(align_items="start")
+        self.other_page_row = ui.row(align_items="start")
+        self.other_page_row.set_visibility(False)
+        self.pagination.on_value_change(
+            lambda: self.on_page_change(self.pagination.value)
+        )
+        self.pagination.bind_enabled_from(
+            GlobalStatus,
+            "task_running",
+            lambda x: x == Status.FAILED or x == Status.SUCCEEDED,
+        )
 
-        maafw.notification_handler = self.notification_handler
+        if PER_PAGE_ITEM_NUM is None:
+            self.pagination.set_visibility(False)
+
+    async def on_reverse_switch_change(self, value: bool):
+        await self.clear()
+        STORAGE["items-reverse"] = value
 
     async def clear(self):
         await maafw.clear_cache()
         self.clear_items()
+        self.data.clear()
 
     def clear_items(self):
-        self.row.clear()
         self.row_len = 0
+        self.data.clear()
+        self.list_data_map.clear()
 
+        self.homepage_row.clear()
+        self.other_page_row.clear()
+        self.pagination.max = 1
+        self.pagination.set_value(1)
+
+    def on_page_change(self, page: int):
+        if PER_PAGE_ITEM_NUM is None:
+            return
+
+        self.other_page_row.clear()
+
+        if page == 1:
+            self.homepage_row.set_visibility(True)
+            self.other_page_row.set_visibility(False)
+        else:
+            self.homepage_row.set_visibility(False)
+            self.other_page_row.set_visibility(True)
+
+            # Get all row_len in reverse order
+            row_len_list = range(self.row_len, 0, -1)
+            total_items = len(row_len_list)
+            start_index = max((page - 1) * PER_PAGE_ITEM_NUM, 0)
+            end_index = min(start_index + PER_PAGE_ITEM_NUM, total_items)
+
+            # Get the row_len for the current page
+            # NOTE: This row_len_list is reversed
+            for row_len in row_len_list[start_index:end_index]:
+                self.create_list(self.other_page_row, self.list_data_map[row_len])
+
+    # maafw
+    def on_recognized(self, reco_id: int, name: str, hit: bool):
+        target_item = None
+        for item in self.data[self.row_len].values():
+            if item.status == Status.PENDING and item.name == name:
+                target_item = item
+                break
+
+        if not target_item:
+            return
+
+        target_item.reco_id = reco_id
+        target_item.status = hit and Status.SUCCEEDED or Status.FAILED
+
+        RecoData.data[reco_id] = name, hit
+        asyncio.run(maafw.screenshotter.refresh(False))
+
+    # maafw
     def on_next_list_starting(self, current: str, list_to_reco: list[str]):
-        self.row_len = self.row_len + 1
+        self.row_len += 1
 
-        self.cur_list = list_to_reco
-        self.next_reco_index = 0
+        list_data = ListData(self.row_len, current, list_to_reco)
+        self.add_list_data(list_data)
 
-        with self.row:
-            self._add_list(current, list_to_reco)
+        # 299/300 -> page:1 | 300/300 -> page:2
+        if (
+            PER_PAGE_ITEM_NUM is not None
+            and self.row_len / PER_PAGE_ITEM_NUM >= self.pagination.max
+        ):
+            self.pagination.max += 1
+            self.homepage_row.clear()
+
+        # Display warning when pagination is disabled
+        # In the future, we can consider to enable pagination by default
+        if PER_PAGE_ITEM_NUM is None and ITEM_NUMBER_WARING is not None:
+            if self.row_len == ITEM_NUMBER_WARING:
+                with self.homepage_row:
+                    ui.html(
+                        "<style>.multi-line-notification { white-space: pre-line; }</style>"
+                    )  # allow use \n in notification text
+                    ui.notification(
+                        f"Item number is reaching the {ITEM_NUMBER_WARING}! Please consider to enable pagination.\nFor more information, please see {PAGINATION_DOCS_URL}",
+                        position="bottom-right",
+                        type="warning",
+                        timeout=None,
+                        close_button=True,
+                        multi_line=True,
+                        classes="multi-line-notification",
+                        actions=[
+                            {
+                                "label": "VIEW",
+                                "color": "white",
+                                ":handler": f"() => emitEvent('pagination-docs-clicked')",
+                            }
+                        ],
+                    )
+                    ui.on(
+                        "pagination-docs-clicked",
+                        lambda: ui.navigate.to(PAGINATION_DOCS_URL, new_tab=True),
+                    )
+
+        self.create_list(self.homepage_row, list_data)
 
         asyncio.run(maafw.screenshotter.refresh(False))
 
-    def _add_list(self, current: str, list_to_reco: list[str]):
-        with ui.list().props("bordered separator") as ls:
-            ui.item_label(current).props("header").classes("text-bold")
-            ui.separator()
+    def add_list_data(self, data: ListData):
+        self.list_data_map[data.row_len] = data
+        for index in range(len(data.list_to_reco)):
+            name = data.list_to_reco[index]
+            self.add_item_data(index, name, data.row_len)
 
-            for index in range(len(list_to_reco)):
-                name = list_to_reco[index]
-                self._add_item(index, name)
+    def add_item_data(self, index, name, row_len: int):
+        data = ItemData(row_len, index, name)
+        self.data[row_len][index] = data
 
-        ls.move(self.row, 0)  # the key of reverse
+    def create_list(self, row: ui.row, data: ListData):
+        reverse: bool = self.reverse_switch.value
 
-    @bindable_dataclass
-    class ItemData:
-        col: int
-        row: int
-        name: str
-        reco_id: int = 0
-        status: Status = Status.PENDING
+        with row:
+            with ui.list().props("bordered separator") as ls:
+                ls.set_visibility(False)  # The list will be hidden until prepared
 
-    def _add_item(self, index, name):
-        data = RecognitionRow.ItemData(self.row_len, index, name)
-        self.data[self.row_len][index] = data
+                # reverse
+                if row == self.homepage_row and reverse:
+                    ls.move(row, 0)
+                elif row == self.other_page_row and not reverse:
+                    # As
+                    # When in other page, we need to reverse the reverse logic
+                    ls.move(row, 0)
+
+                ui.item_label(data.current).props("header").classes("text-bold")
+                ui.separator()
+
+                for index in range(len(data.list_to_reco)):
+                    name = data.list_to_reco[index]
+                    self.create_items(index, name, data.row_len)
+
+                ls.set_visibility(True)
+
+    def create_items(self, index: int, name: str, row_len: int):
+        data: ItemData = self.data[row_len][index]
 
         with ui.item(on_click=lambda data=data: self.on_click_item(data)):  # type: ignore
             with ui.item_section().props("side"):
@@ -133,28 +292,10 @@ class RecognitionRow:
                 ).props("caption")
 
     def on_click_item(self, data: ItemData):
-        print(f"on_click_item ({data.col}, {data.row}): {data.name} ({data.reco_id})")
-
-        ui.navigate.to(f"reco/{data.reco_id}", new_tab=True)
-
-    def on_recognized(self, reco_id: int, name: str, hit: bool):
-        print(f"on_recognized: {reco_id}, {name}, {hit}")
-
-        target = None
-        for item in self.data[self.row_len].values():
-            if item.status == Status.PENDING and item.name == name:
-                target = item
-                break
-
-        if not target:
+        if data.reco_id == 0:
             return
-
-        target.reco_id = reco_id
-        target.status = hit and Status.SUCCEEDED or Status.FAILED
-
-        RecoData.data[reco_id] = name, hit
-        asyncio.run(maafw.screenshotter.refresh(False))
-
-
-class Controls:
-    recognition_row = RecognitionRow()
+        else:
+            print(
+                f"on_click_item ({data.col}, {data.row}): {data.name} ({data.reco_id})"
+            )
+            ui.navigate.to(f"reco/{data.reco_id}", new_tab=True)
