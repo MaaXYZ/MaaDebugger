bb619c952e20b91e8ca0203693db93e38406dc96
weinibuliu <weinibuliu@outlook.com>
2025-07-01 19:24:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from nicegui import ui, app
 
 from src.MaaDebugger import MaaDebugger
 
-ui.timer(3, lambda: app.shutdown(), once=True)
+ui.timer(10, lambda: app.shutdown(), once=True)
 
-MaaDebugger.run_in_cli()
+MaaDebugger.run()
