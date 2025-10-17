95ee0d02512f43e1ff1329996d3d082f72f26dd1
weinibuliu <weinibuliu@outlook.com>
2025-10-17 16:58:48 +0800

    fix: 修复 on_exception 未使用的问题

--- src/MaaDebugger/__init__.py
+++ src/MaaDebugger/__init__.py
@@ -2,11 +2,12 @@ from pathlib import Path
 from typing import Optional
 
 
-from nicegui import ui
+from nicegui import app, ui
 from nicegui.native.native_mode import find_open_port
 
 from .webpage import index_page
 from .webpage import reco_page  # noqa: F401
+from .webpage.traceback_page import on_exception
 from .utils import update_checker
 from .maafw import maafw
 from .__version__ import version
@@ -45,8 +46,8 @@ class MaaDebugger:
             print("WARNING: All devices in the LAN can access MaaDebugger.")
 
         index_page.main()
-
         ui.timer(2, update_checker.main, once=True)  # Check update
+        app.on_exception(on_exception)
 
         ui.run(
             title=APP_TITLE,
