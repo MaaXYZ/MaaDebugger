e29b225518dfad9545daebb552d50f9c6736a42a
weinibuliu <weinibuliu@outlook.com>
2026-02-24 15:02:59 +0800

    fix: 仅在必要时自动重连 agent

--- src/MaaDebugger/webpage/index_page/master_control.py
+++ src/MaaDebugger/webpage/index_page/master_control.py
@@ -637,9 +637,9 @@ def run_task_control():
             GlobalStatus.task_running = Status.FAILED
             return
 
-        # 重新加载资源 / 连接 Agent
+        # 重新加载资源 / 连接 Agent(如果有)
         res_status = await on_click_resource_load(STORAGE.get("resource_dir"))
-        agent_status = await on_click_agent()
+        agent_status = await on_click_agent() if maafw.agent else True
 
         if not (res_status and agent_status):
             GlobalStatus.task_running = Status.FAILED
