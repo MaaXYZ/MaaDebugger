6135d9b04242b350392a2b42119541e3547aee31
weinibuliu <weinibuliu@outlook.com>
2026-03-09 16:52:57 +0800

    fix: fps 默认值修改

--- web/src/stores/screenshot.ts
+++ web/src/stores/screenshot.ts
@@ -3,6 +3,6 @@ import { ref, shallowRef } from "vue";
 export const latestFrame = shallowRef<ArrayBuffer | null>(null);
 export const screenshotRunning = ref(false);
 export const screenshotPaused = ref(false);
-export const screenshotFps = ref(30);
+export const screenshotFps = ref(15);
 export const screenshotActualFps = ref(0);
 export const screenshotError = ref("");
