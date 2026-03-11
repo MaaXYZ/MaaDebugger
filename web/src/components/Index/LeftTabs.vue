5b1ec34a7cc81e2b4cd1b2e997294c049bb54308
weinibuliu <weinibuliu@outlook.com>
2026-03-11 12:01:05 +0800

    perf: 左侧布局修改

---
+++ web/src/components/Index/LeftTabs.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="gap-2 flex flex-col">
+        <UTabs :items="tabs" key="value" v-model="tabsModel" />
+        <div v-show="tabsModel === 'controller'" class="gap-2 flex flex-col">
+            <ControllerCard />
+        </div>
+        <div v-show="tabsModel === 'resource'" class="gap-2 flex flex-col">
+            <InterfaceCard />
+            <ResourceCard />
+        </div>
+        <div v-show="tabsModel === 'agent'" class="gap-2 flex flex-col">
+            <AgentCard />
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { type TabsItem } from '@nuxt/ui';
+
+import InterfaceCard from './InterfaceCard.vue';
+import ControllerCard from './ControllerCard.vue';
+import ResourceCard from './ResourceCard.vue';
+import AgentCard from './AgentCard.vue';
+
+const tabsModel = ref("controller")
+const tabs: TabsItem[] = [
+    { label: 'Controller', value: 'controller' },
+    { label: 'Resource', value: 'resource' },
+    { label: 'Agent', value: 'agent' }
+];
+</script>
--- web/src/router/Index.vue
+++ web/src/router/Index.vue
@@ -2,10 +2,7 @@
     <div class="flex flex-col lg:grid lg:grid-cols-3 items-start w-full gap-4 p-4 lg:p-6">
         <!-- 第一列: Interface / Controller / Resource / Agent -->
         <div class="w-full flex flex-col gap-4 order-4 lg:order-0">
-            <InterfaceCard />
-            <ControllerCard />
-            <ResourceCard />
-            <AgentCard />
+            <LeftTabs />
         </div>
         <!-- 第二列: Task (includes Screenshot) -->
         <div class="w-full flex flex-col gap-4 order-2 lg:order-0">
@@ -26,10 +23,7 @@
 </template>
 
 <script setup lang="ts">
-import InterfaceCard from "@/components/Index/InterfaceCard.vue";
-import ControllerCard from "@/components/Index/ControllerCard.vue";
-import ResourceCard from "@/components/Index/ResourceCard.vue";
-import AgentCard from "@/components/Index/AgentCard.vue";
+import LeftTabs from "@/components/Index/LeftTabs.vue";
 import TaskCard from "@/components/Index/TaskCard.vue";
 import TaskDetailCard from "@/components/Index/TaskDetailCard.vue";
 </script>
