6ea1968730b697b0f5e13c71bc955d37acdcd279
weinibuliu <weinibuliu@outlook.com>
2026-03-12 19:45:07 +0800

    chore: python entry point

--- publish/pypi/__main__.py
+++ publish/pypi/__main__.py
@@ -5,43 +5,51 @@ import importlib.util
 import subprocess
 from pathlib import Path
 
-spec = importlib.util.find_spec("maa")
-if spec and spec.origin:
-    module_path = spec.origin
-    LIB_PATH = Path(module_path).parent / "bin"
-else:
-    print("Please run `pip install maafw`")
-    raise ModuleNotFoundError("Module 'maa' is not found")
 
-OS_TYPE = platform.system()
-EXE_PATH = Path(__file__).parent
-if OS_TYPE == "Windows":
-    EXE_PATH = EXE_PATH / "MaaDebugger.exe"
-else:
-    EXE_PATH = EXE_PATH / "MaaDebugger"
+def main() -> int:
+    spec = importlib.util.find_spec("maa")
+    if spec and spec.origin:
+        module_path = spec.origin
+        LIB_PATH = Path(module_path).parent / "bin"
+    else:
+        print("Please run `pip install maafw`")
+        raise ModuleNotFoundError("Module 'maa' is not found")
 
-if not LIB_PATH.is_dir():
-    raise FileNotFoundError(f"Library path not found: {LIB_PATH}")
-if not EXE_PATH.is_file():
-    raise FileNotFoundError(f"Executable not found: {EXE_PATH}")
+    OS_TYPE = platform.system()
+    EXE_PATH = Path(__file__).parent
+    if OS_TYPE == "Windows":
+        EXE_PATH = EXE_PATH / "MaaDebugger.exe"
+    else:
+        EXE_PATH = EXE_PATH / "MaaDebugger"
 
-ENV = {
-    **os.environ,
-    "MAADBG_CHANNEL": "pypi",
-    "MAADBG_CHANNEL_MANAGER": "pypi",
-    "MAADBG_CHANNEL_LIB_PATH": str(LIB_PATH),
-}
+    if not LIB_PATH.is_dir():
+        raise FileNotFoundError(f"Library path not found: {LIB_PATH}")
+    if not EXE_PATH.is_file():
+        raise FileNotFoundError(f"Executable not found: {EXE_PATH}")
 
-try:
-    result = subprocess.run(
-        [str(EXE_PATH), *sys.argv[1:]],
-        env=ENV,
-        shell=False,
-        check=False,
-    )
-    sys.exit(result.returncode)
-except Exception as e:
-    print(f"System: {OS_TYPE}", file=sys.stderr)
-    print(f"Lib Path: {LIB_PATH}", file=sys.stderr)
-    print(f"Exe Path: {EXE_PATH}", file=sys.stderr)
-    raise e
+    ENV = {
+        **os.environ,
+        "MAADBG_CHANNEL": "pypi",
+        "MAADBG_CHANNEL_MANAGER": "pypi",
+        "MAADBG_CHANNEL_LIB_PATH": str(LIB_PATH),
+    }
+
+    try:
+        result = subprocess.run(
+            [str(EXE_PATH), *sys.argv[1:]],
+            env=ENV,
+            shell=False,
+            check=False,
+        )
+    except Exception as e:
+        print(f"System: {OS_TYPE}", file=sys.stderr)
+        print(f"Lib Path: {LIB_PATH}", file=sys.stderr)
+        print(f"Exe Path: {EXE_PATH}", file=sys.stderr)
+        raise e
+
+    finally:
+        sys.exit(result.returncode)
+
+
+if __name__ == "__main__":
+    main()
