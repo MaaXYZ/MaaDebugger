d8a52c7c7934075804f4dda5d63703e602d5b6f1
weinibuliu <weinibuliu@outlook.com>
2026-03-16 19:13:59 +0800

    fix: BuildTime 被错误覆盖的问题

--- server/internal/buildinfo/buildinfo.go
+++ server/internal/buildinfo/buildinfo.go
@@ -1,13 +1,8 @@
 package buildinfo
 
-import (
-	"fmt"
-	"time"
-)
-
 // 通过 -ldflags 在编译时注入相关信息
 var (
 	Version   string = "dev"
 	CommitSHA string = "dev"
-	BuildTime string = fmt.Sprintf("%d", time.Now().Unix())
+	BuildTime string = "0"
 )
