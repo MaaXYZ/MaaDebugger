c8c60c9e894a982505fe6f95fe1ad77cd0dcc3b6
weinibuliu <weinibuliu@outlook.com>
2026-04-05 14:00:29 +0800

    chore: index class name

--- web/src/router/Index.vue
+++ web/src/router/Index.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="w-full min-h-full flex flex-col gap-4 p-4 lg:p-6">
-        <div class="flex items-center justify-between gap-3">
+        <div class="items-center justify-between gap-3 hidden xl:flex">
             <UButton color="neutral" variant="ghost" size="sm"
                 :icon="showLeftTabs ? 'i-lucide-panel-left-close' : 'i-lucide-panel-left-open'"
                 :label="showLeftTabs ? 'Hide Setup' : 'Show Setup'" @click="toggleLeftTabs" />
         </div>
 
         <div class="flex flex-col xl:flex-row xl:items-start">
-            <div class="order-3 min-w-0 overflow-hidden motion-reduce:transition-none xl:order-0 xl:shrink-0 xl:w-[clamp(260px,28vw,32rem)]"
-                :class="showLeftTabs
-                    ? 'mb-4 max-h-800 max-w-full opacity-100 transition-[max-height,opacity,margin-bottom] duration-200 ease-out xl:mb-0 xl:mr-4 xl:max-h-none xl:max-w-lg xl:transition-[max-width,opacity,margin-right]'
-                    : 'pointer-events-none mb-0 max-h-0 max-w-0 opacity-0 transition-[max-height,opacity,margin-bottom] duration-150 ease-in xl:mr-0 xl:max-h-none xl:transition-[max-width,opacity,margin-right]'">
+            <div class="order-3 min-w-0 overflow-hidden transition-all duration-200 ease-out motion-reduce:transition-none xl:order-0 xl:shrink-0 xl:w-[clamp(260px,28vw,32rem)]
+                       mb-4 max-h-800 max-w-full opacity-100 xl:mb-0 xl:mr-4 xl:max-h-none xl:max-w-lg
+                       data-[collapsed=true]:mb-0 data-[collapsed=true]:max-h-0 data-[collapsed=true]:max-w-0 data-[collapsed=true]:opacity-0 data-[collapsed=true]:pointer-events-none data-[collapsed=true]:duration-150 data-[collapsed=true]:ease-in data-[collapsed=true]:xl:mr-0"
+                :data-collapsed="!showLeftTabs">
                 <LeftTabs />
             </div>
 
@@ -18,7 +18,6 @@
                 <div class="w-full min-w-0 flex flex-col gap-4 xl:self-start">
                     <TaskCard />
                 </div>
-
                 <div class="w-full min-w-0 flex flex-col gap-4 xl:self-start">
                     <TaskDetailCard />
                 </div>
