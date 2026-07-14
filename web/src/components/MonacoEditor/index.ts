b54c9e6b7d8b83bf02de2fb59b7838a7d1e9a593
weinibuliu <weinibuliu@outlook.com>
2026-07-14 17:19:18 +0800

    fix: monaco editor 懒加载

--- web/src/components/MonacoEditor/index.ts
+++ web/src/components/MonacoEditor/index.ts
@@ -1,6 +1,5 @@
 export { default as MonacoEditor } from "./MonacoEditor.vue";
 export type { MonacoEditorProps } from "./MonacoEditor.vue";
-export { default as JsonEditorModal } from "./JsonEditorModal.vue";
 export {
   monaco,
   ensureMonacoReady,
